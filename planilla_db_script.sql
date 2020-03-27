c8de9b7f910abba615646de1d11a7344ca7b5a7b
EngineerDV <engdv2017@gmail.com>
2020-03-27 09:45:56 -0600

    CORRIGIENDO CORRELATIVO

--- planilla_db_script.sql
+++ planilla_db_script.sql
@@ -1,32 +1,32 @@
 CREATE DATABASE planilla_db;
 USE planilla_db;
 
-CREATE TABLE Concepto
+CREATE TABLE Concepto_19543
 (codigo_concepto varchar(5) primary key not null,
 nombre_concepto varchar(60) not null,
 efecto_concepto varchar(1) not null,
 estatus_concepto varchar(1) not null
 )engine=InnoDB default charset=latin1;
 
-CREATE TABLE Departamento
+CREATE TABLE Departamento_19543
 (codigo_departamento varchar(5) primary key not null,
 nombre_departamento varchar(60) not null,
 estatus_departamento varchar(1) not null
 )engine=InnoDB default charset=latin1;
 
-CREATE TABLE Puesto
+CREATE TABLE Puesto_19543
 (codigo_puesto varchar(5) primary key not null, 
 nombre_puesto varchar(60) not null,
 estatus_puesto varchar(1) not null
 )engine=InnoDB default charset=latin1;
 
-CREATE TABLE NominaE
+CREATE TABLE NominaE_19543
 (codigo_nomina varchar(5) primary key not null,
 fecha_inicial_nomina date not null,
 fecha_final_nomina date not null
 )engine=InnoDB default charset=latin1;
 
-CREATE TABLE NominasD
+CREATE TABLE NominasD_19543
 (
 FK_CodigoNomina Varchar(5) not null,
 Fk_CodigoEmpleado Varchar(5) not null,
@@ -34,7 +34,7 @@ FK_CodigoConcepto varchar(5) not null,
 valor_nominaD float(10,2) not null
 )engine=InnoDB default charset=latin1;
 
-CREATE TABLE Empleado
+CREATE TABLE Empleado_19543
 (codigo_empleado varchar(5) primary key not null,
 nombre_empleado varchar(60) not null,
 FKcodigo_puesto varchar(5) not null,
